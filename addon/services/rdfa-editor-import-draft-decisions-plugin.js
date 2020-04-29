18035a7e7a5c8a5aa07f89903124061d03a047f9
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-04-29 12:40:00 +0200

    Don't use selectContext for every incoming block
    This is currently very expensive (altough very nice) and affects the
    performance much.
    Needs evolution later

--- addon/services/rdfa-editor-import-draft-decisions-plugin.js
+++ addon/services/rdfa-editor-import-draft-decisions-plugin.js
@@ -8,17 +8,17 @@ import isEmpty from '@lblod/ember-rdfa-editor/utils/is-empty'
 export default class RdfaEditorImportDraftDecisionsPluginService extends Service {
   editorApi = "0.1"
   @service store;
+  draftDecisionProperty = 'http://mu.semte.ch/vocabularies/ext/draftDecisionsHint'
 
   async execute(rdfaBlocks, hintsRegistry, editor, extraInfo) {
     hintsRegistry.removeHints( { rdfaBlocks, scope: "import-draft-decisions" } );
 
     for (const block of rdfaBlocks) {
       const location = block.region;
-      const selection = editor.selectContext( block.region, {
-        property: "http://mu.semte.ch/vocabularies/ext/draftDecisionsHint"
-      });
 
-      if (!isEmpty(selection)) {
+      const contextResult = this.detectRelevantContext(block);
+
+      if (contextResult) {
         hintsRegistry.addHint( "import-draft-decisions", {
           location,
           card: "editor-plugins/import-draft-decisions-card",
@@ -28,5 +28,15 @@ export default class RdfaEditorImportDraftDecisionsPluginService extends Service
         });
       }
     }
+  }
+
+  detectRelevantContext({ semanticNode }) {
+    if (semanticNode.rdfaAttributes && semanticNode.rdfaAttributes.properties) {
+      const properties = semanticNode.rdfaAttributes.properties || A();
+      if (properties.includes(this.draftDecisionProperty)) {
+        return semanticNode;
+      }
+    }
+    return null;
   }
 }
